4b4c2f482d5511e878c97314e2d84fcfa11b221b
Andreé Sandoval <andree.bit@gmail.com>
2023-05-13 19:41:30 -0500

    refactor: move route handling to bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Lib\Routing\Handler;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../routes/web.php';
 require_once __DIR__ . '/lib/Routing/Handler.php';
+
+(new Handler($router, $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']))->handle();
--- public/index.php
+++ public/index.php
@@ -1,7 +1,3 @@
 <?php
 
-use App\Lib\Routing\Handler;
-
 require_once __DIR__ . '/../app/bootstrap.php';
-
-(new Handler($router, $_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']))->handle();
